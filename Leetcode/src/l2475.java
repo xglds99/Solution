765505344e77fc0e0c61a6e8a15b7306bd73af83
xgl <2432748713@qq,com>
2023-06-13 11:22:35 +0800

    qq

--- Leetcode/src/l2475.java
+++ Leetcode/src/l2475.java
@@ -1,6 +1,7 @@
 import java.nio.IntBuffer;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class l2475 {
@@ -14,6 +15,21 @@ public class l2475 {
                 res += start * (i - start + 1) * (n - 1 -i);
                 start = i + 1;
             }
+        }
+        return res;
+    }
+
+    public int unequalTriplets_1(int[] nums) {
+        Map<Integer, Long> map = Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.
+                        groupingBy(i -> i, Collectors.counting()));
+        int res = 0, a =0, c = nums.length;
+        for (var entry: map.entrySet()){
+            int b = Math.toIntExact(entry.getValue());
+            c -= b;
+            res += a * b * c;
+            a += b;
         }
         return res;
     }
